30772f3243ef16f52d7929006350b8fe28e9d3d2
TomAndy <andrufly@ukr.net>
2017-06-05 23:42:13 +0300

    Updated MessageHelper file

--- src/test/java/com/onlineshop/utils/MessageHelper.java
+++ src/test/java/com/onlineshop/utils/MessageHelper.java
@@ -1,7 +1,12 @@
 package com.onlineshop.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class MessageHelper {
 
@@ -9,8 +14,40 @@ public class MessageHelper {
     private static final Map<String, String> codeToMessages = new HashMap<String, String>();
 
     public static String getMessageByCode(String code) {
-
         return codeToMessages.get(code);
+    }
+
+    public MessageHelper() {
+        Properties prop = new Properties();
+        String propFileName = "error_codes.properties";
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream(propFileName);
+            prop.load(input);
+
+            Enumeration<?> e = prop.propertyNames();
+            while (e.hasMoreElements()) {
+                String key = (String) e.nextElement();
+                String value = prop.getProperty(key);
+                System.out.println(key+"   "+value);
+                codeToMessages.put(key, value);
+            }
+
+        } catch(IOException e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MessageHelper ms = new MessageHelper();
+        System.out.println(MessageHelper.getMessageByCode("DB_ERR_100"));
     }
 
 }
